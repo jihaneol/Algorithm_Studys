d2a5f893cc288d3881cdc07e9e3733228dc461b3
ji <6531z@naver.com>
2022-11-03 18:04:46 +0900

    22-11-3

---
+++ SWEA/n-Queen 백트레킹 응용.py
@@ -0,0 +1,189 @@
+# def promising(x,y):
+#     for i in range(8):
+#         nx=dx[i]+x
+#         ny=dy[i]+y
+#         while nx>=0 and ny>=0 and ny<n and nx<n:
+#             if data[nx][ny]==1:
+#                 return False
+#             nx+=dx[i]
+#             ny+=dy[i]
+    
+#     return True
+
+# T = int(input())
+# # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+#     answer=0
+#     n=int(input())
+#     data=[]
+#     queen=[]
+#     dx=[0,1,1,1,0,-1,-1,-1]
+#     dy=[1,1,0,-1,-1,-1,0,1]
+#     for i in range(n):
+#         data.append([0]*n)
+#         for j in range(n):
+#             queen.append((i,j))
+#     visited={} # 가지치기
+#     q=[]
+#     def solution(cnt):
+#         global answer
+#         if cnt==n:
+#             temp=[]
+#             for i in range(n):
+#                 for j in range(n):
+#                     if data[i][j]==1:
+#                         temp.append((i,j))
+
+
+#             for x,y in temp:
+#                 if promising(x,y):
+#                     continue
+#                 else:
+#                     return
+#             print(data)
+#             answer+=1
+
+#         else:
+#             for i in range(n):
+#                 for j in range(n):
+                    
+
+#                     if visited.get((i,j),1):
+#                         visited[(i,j)]=0
+#                         data[i][j]=1
+#                         solution(cnt+1)
+#                         data[i][j]=0
+#                         visited[(i,j)]=1
+#     solution(0)
+#     print(answer)
+
+
+
+# #D3 2806 N-Queen
+ 
+# #대각선 체크 함수
+# def possible(idx, c):
+#     for i in range(idx):
+#         #행과 열의 차이가 같다면
+#         if idx - i == abs(c - map_list[i]): return True
+#     return False
+    
+# def dfs(idx):
+#     if idx == N:
+#         global answer
+#         answer += 1
+#         return
+#     for i in range(N):
+#         #이미 사용한 열이라면 넘어감
+#         if visit[i] : continue
+#         #대각선이 겹친다면 넘어감
+#         if possible(idx, i) : continue
+#         visit[i] = 1
+#         map_list[idx] = i
+#         dfs(idx + 1)
+#         visit[i] = 0
+ 
+# for t in range(1, int(input()) + 1):
+#     N = int(input())
+#     map_list = [0 for _ in range(N)]
+#     visit = [0 for _ in range(N)]
+#     answer = 0
+#     dfs(0)
+#     print('#{} {}'.format(t, answer))
+
+# 수정
+def promising(x,y):
+    for i in range(8):
+        nx=dx[i]+x
+        ny=dy[i]+y
+        while nx>=0 and ny>=0 and ny<n and nx<n:
+            if data[nx][ny]==1:
+                return False
+            nx+=dx[i]
+            ny+=dy[i]
+    
+    return True
+
+
+def solution(cnt):
+        global answer
+        if cnt==n:
+            answer+=1
+            return
+        else:
+            for i in range(n):
+                if visited.get((cnt,i),0):
+                    continue
+                else:
+                    visited[(cnt,i)]=1
+                    data[cnt][i]=1
+
+                    if promising(cnt,i):
+                        solution(cnt+1)
+
+                    data[cnt][i]=0
+                    visited[(cnt,i)]=0
+
+#최종
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    answer=0
+    n=int(input())
+    data=[]
+    queen=[]
+    dx=[0,1,1,1,0,-1,-1,-1]
+    dy=[1,1,0,-1,-1,-1,0,1]
+    for i in range(n):
+        data.append([0]*n)
+
+    visited={} # 가지치기
+    q=[]
+    
+    solution(0)
+    print(answer)
+
+
+
+
+def promising(x,y):
+    for i in range(6):
+        nx=dx[i]+x
+        ny=dy[i]+y
+        while nx>=0 and ny>=0 and ny<n and nx<n:
+            if data[nx][ny]==1:
+                return False
+            nx+=dx[i]
+            ny+=dy[i]
+    
+    return True
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    answer=0
+    n=int(input())
+    data=[]
+    queen=[]
+    dx=[1,1,1,-1,-1,-1]
+    dy=[1,0,-1,-1,0,1]
+    for i in range(n):
+        data.append([0]*n)
+    def solution(cnt):
+        global answer
+        if cnt==n:
+            answer+=1
+            return
+        else:
+            for i in range(n):
+                
+                data[cnt][i]=1
+
+                if promising(cnt,i):
+                    solution(cnt+1)
+                data[cnt][i]=0
+                
+
+
+    solution(0)
+    print(f'#{test_case} {answer}')
--- SWEA/최대상금_다이나믹이용.py
+++ SWEA/최대상금_다이나믹이용.py
@@ -1,92 +1,309 @@
-# def solution(p,cnt):
-#     if cnt==x:
-#         return p
+# def promising(x,y):
+#     for i in range(8):
+#         nx=dx[i]+x
+#         ny=dy[i]+y
+#         while nx>=0 and ny>=0 and ny<n and nx<n:
+#             if data[nx][ny]==1:
+#                 return False
+#             nx+=dx[i]
+#             ny+=dy[i]
     
-#     if p[0]!=max(p):
-#         if len(p)==2:
-#             p[1],p[0] = p[0], p[1]
-#             return solution(p,cnt+1)
-#         p[len(p)-p[::-1].index(max(p))-1],p[0] = p[0], p[len(p)-p[::-1].index(max(p))-1]
-#         return [p[0]] + solution(p[1:],cnt+1) 
-    
-#     if p[0]==max(p):
-#         if len(p)==2:
-#             p[1],p[0] = p[0], p[1]
-#             return solution(p,cnt+1)
-#         return [p[0]] + solution(p[1:],cnt) 
+#     return True
 
-def solution(p,cnt):
-    global answer
-    length = len(p)
-
-    if cnt==x:
-        a=''.join(list(map(str,p)))
-        if answer <= int(a):
-            answer= int(a)
-        return 
-    
-    for i in range(length-1):
-        for j in range(i+1,length):
-            p[j],p[i] = p[i],p[j]
-            a=''.join(list(map(str,p)))
-            if visited.get((a,cnt+1),1):
-                solution(p,cnt+1)
-                visited[(a,cnt)]=0
-            p[i],p[j] = p[j], p[i]
+# T = int(input())
+# # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+#     answer=0
+#     n=int(input())
+#     data=[]
+#     queen=[]
+#     dx=[0,1,1,1,0,-1,-1,-1]
+#     dy=[1,1,0,-1,-1,-1,0,1]
+#     for i in range(n):
+#         data.append([0]*n)
+#         for j in range(n):
+#             queen.append((i,j))
+#     visited={} # 가지치기
+#     q=[]
+#     def solution(cnt):
+#         global answer
+#         if cnt==n:
+#             temp=[]
+#             for i in range(n):
+#                 for j in range(n):
+#                     if data[i][j]==1:
+#                         temp.append((i,j))
 
 
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    answer=0
-    
-    n,x=map(int,input().split())
-    n=list(map(int,str(n)))
-    visited={}
-    solution(n,0)
-    print(f'#{test_case} {answer}')
+#             for x,y in temp:
+#                 if promising(x,y):
+#                     continue
+#                 else:
+#                     return
+#             print(data)
+#             answer+=1
+
+#         else:
+#             for i in range(n):
+#                 for j in range(n):
+                    
+
+#                     if visited.get((i,j),1):
+#                         visited[(i,j)]=0
+#                         data[i][j]=1
+#                         solution(cnt+1)
+#                         data[i][j]=0
+#                         visited[(i,j)]=1
+#     solution(0)
+#     print(answer)
 
 
-#D3 1244 최대상금
+
+#D3 2806 N-Queen
  
-#경우의 수 찾기, 매개변수로 몇번 바꿧는지 적는다.
-def dfs(count):
-    global answer
-    #횟수를 다 사용했다면
-    if not count:
-        #숫자로 바꿔보고
-        temp = int(''.join(values))
-        #가지고 있는 최대수보다 크다면 갱신
-        if answer < temp:
-            answer = temp
+# #대각선 체크 함수
+# def possible(idx, c):
+#     for i in range(idx):
+#         #행과 열의 차이가 같다면
+#         if idx - i == abs(c - map_list[i]): return True
+#     return False
+    
+# def dfs(idx):
+#     if idx == N:
+#         global answer
+#         answer += 1
+#         return
+#     for i in range(N):
+#         #이미 사용한 열이라면 넘어감
+#         if visit[i] : continue
+#         #대각선이 겹친다면 넘어감
+#         if possible(idx, i) : continue
+#         visit[i] = 1
+#         map_list[idx] = i
+#         dfs(idx + 1)
+#         visit[i] = 0
+ 
+# for t in range(1, int(input()) + 1):
+#     N = int(input())
+#     map_list = [0 for _ in range(N)]
+#     visit = [0 for _ in range(N)]
+#     answer = 0
+#     dfs(0)
+#     print('#{} {}'.format(t, answer))
+
+# 수정
+# def promising(x,y):
+#     for i in range(8):
+#         nx=dx[i]+x
+#         ny=dy[i]+y
+#         while nx>=0 and ny>=0 and ny<n and nx<n:
+#             if data[nx][ny]==1:
+#                 return False
+#             nx+=dx[i]
+#             ny+=dy[i]
+    
+#     return True
+
+
+# def solution(cnt):
+#         global answer
+#         if cnt==n:
+#             answer+=1
+#             return
+#         else:
+#             for i in range(n):
+#                 if visited.get((cnt,i),0):
+#                     continue
+#                 else:
+#                     visited[(cnt,i)]=1
+#                     data[cnt][i]=1
+
+#                     if promising(cnt,i):
+#                         solution(cnt+1)
+
+#                     data[cnt][i]=0
+#                     visited[(cnt,i)]=0
+
+# #최종
+# T = int(input())
+# # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+#     answer=0
+#     n=int(input())
+#     data=[]
+#     queen=[]
+#     dx=[0,1,1,1,0,-1,-1,-1]
+#     dy=[1,1,0,-1,-1,-1,0,1]
+#     for i in range(n):
+#         data.append([0]*n)
+
+#     visited={} # 가지치기
+#     q=[]
+    
+#     solution(0)
+#     print(answer)
+
+
+
+
+# def promising(x,y):
+#     for i in range(6):
+#         nx=dx[i]+x
+#         ny=dy[i]+y
+#         while nx>=0 and ny>=0 and ny<n and nx<n:
+#             if data[nx][ny]==1:
+#                 return False
+#             nx+=dx[i]
+#             ny+=dy[i]
+    
+#     return True
+
+# T = int(input())
+# # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+#     answer=0
+#     n=int(input())
+#     data=[]
+#     queen=[]
+
+#     dx=[1,1,1,-1,-1,-1]
+#     dy=[1,0,-1,-1,0,1]
+#     for i in range(n):
+#         data.append([0]*n)
+#     def solution(cnt):
+#         global answer
+#         if cnt==n:
+#             answer+=1
+#             return
+#         else:
+            
+
+#             for i in range(n):
+                
+#                 data[cnt][i]=1
+
+#                 if promising(cnt,i):
+#                     solution(cnt+1)
+#                 data[cnt][i]=0
+                
+
+
+#     solution(0)
+#     print(f'#{test_case} {answer}')
+# def promising(i,col):
+#     d=1
+#     flag=True
+#     while d<i and flag:
+#         if col[d]==col[i] or abs(col[d]-col[i])==i-d:
+#             flag=False
+#         d+=1
+#     return flag
+
+
+
+# def n_queens(i,col):
+#     global answer
+#     n=len(col)-1
+
+#     if promising(i,col):
+#         if n==i:
+#             answer+=1
+
+#         else:
+#             for k in range(n):
+                
+#                 col[i+1]=k
+#                 n_queens(i+1,col)
+
+# n=int(input())
+# answer=0
+# col = [0]*(n)
+
+# n_queens(0)
+# print(answer)
+
+# def promising(i):
+#     d=0
+#     flag=True
+#     while d<i and flag:
+#         if col[d]==col[i] or abs(col[d]-col[i])==i-d:
+#             flag=False
+#         d+=1
+#     return flag
+
+
+
+# def n_queens(x):
+#     global answer
+    
+#     if n==x:
+#         answer+=1
+
+#     else:
+#         for k in range(n):
+#             col[x]=k
+#             if promising(x):
+#                 n_queens(x+1)
+
+# n=int(input())
+# answer=0
+# col = [0]*n
+
+# n_queens(0)
+# print(answer)
+
+
+
+
+#수정 대칭성 활용
+
+n = int(input())
+
+ans = 0
+row = [0] * n
+sum=0
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    
+    return True
+
+def n_queens(x):
+    global ans,sum
+   
+
+
+    if x == n:
+
+        ans += 1
+        if n%2!=0:
+            if row[0]==(n//2):
+                sum+=1
+
         return
-    # 바꿔야 하니까 이중 포문
-    for i in range(length):
-        #경우의 수를 찾는거니까 i보다 큰위치부터
-        for j in range(i+1, length):
-            #두개의 위치를 바꾸고 나서
-            values[i], values[j] = values[j], values[i]
-            #가지치기 해야하니까 일단 합쳐보고
-            temp_key = ''.join(values)
-            #어떤수가 몇회차에 나왔는지 체크 1이면 안나온거니까 경우의수에 넣어주기
-            if visited.get((temp_key, count-1), 1):
-                #이숫자는 몇회차에 사용했으니까 체크해주고
-                visited[(temp_key, count-1)] = 0
-                #dfs도 돌려주고
-                dfs(count-1)
-            #다 썻으면 원상복귀
-            values[i], values[j] = values[j], values[i]
- 
- 
-for t in range(int(input())):
-    answer = -1
-    value, change = input().split()
-    #바꾸기 편하려고 리스트화시킴
-    values = list(value)
-    change = int(change)
-    #계속 쓸꺼니까 캐스팅
-    length = len(values)
-    #가지치기용 딕셔너리
-    visited = {}
-    dfs(change)
-    print('#{} {}'.format(t+1, answer))
+
+    else:
+        for i in range(n):
+            # [x, i]에 퀸을 놓겠다.
+            row[x] = i
+            
+                
+            if is_promising(x):
+                if n%2==0:
+                    if row[0]==n/2:
+                        break
+                else:
+                    if row[0]==(n//2) +1:
+                        break
+                n_queens(x+1)
+
+n_queens(0)
+
+if n%2==0:
+    print(ans*2)
+else:
+    if n==1:
+        print(1)
+    else:
+        print((ans-sum)*2+sum)
