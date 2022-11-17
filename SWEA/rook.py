b9a98fd2a1fbd29b813da1531556d5b5328594bb
jihaneol <104291422+jihaneol@users.noreply.github.com>
2022-11-17 16:15:41 +0900

    Update rook.py
    다시 확인 하니 쓸데없는 코드가 있었다...

--- SWEA/rook.py
+++ SWEA/rook.py
@@ -36,12 +36,32 @@ for test_case in range(1, T + 1):
                     col[i]=j
                 else:
                     check=False
-                    
-    print(d)
-    
-    
       
-    if check and d==8:
+    if check and d>=8:
         print(f'#{test_case} {solution(0)}')
+    else:
+        print(f'#{test_case} no') 
+	
+#수정
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    col=[-1]*8 # 열 확인
+    row=[-1]*8 행 확인
+    data=[]
+    check=True
+    d=0
+    for i in range(8):
+        data.append(list(input()))
+        for j in range(8):
+            if data[i][j]=='O':
+                d+=1
+                if col[i]==-1 and row[j]==-1:
+                    col[i]=j
+                    row[j]=i
+                else:
+                    check=False
+    if check and d==8:
+        print(f'#{test_case} yes')
     else:
         print(f'#{test_case} no') 
