447e5b7745d3c043ce69a156a3397eeac6c78334
jihaneol <104291422+jihaneol@users.noreply.github.com>
2022-11-17 23:17:51 +0900

    Update 숫자배열.py
    깔끔한 수정

--- SWEA/숫자배열.py
+++ SWEA/숫자배열.py
@@ -1,29 +1,23 @@
-def rotation(arr):
-    r=len(arr)
-    c=len(arr[0])
-    new=[[0]*r for _ in range(c)]
-    for i in range(r):
-        for j in range(c):
-            new[j][r-1-i]=arr[i][j]
-    return new
+#수정
+def rotation():# 90회전
+    temp=[[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            temp[j][n-1-i]=arr[i][j]
+    return temp
 
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    n=int(input())
-    print(f"#{test_case}")
-    data=[]
-    for _ in range(n):
-        data.append(list(map(int,input().split())))
-    cnt=0
-    while cnt<n:
-        for _ in range(3):
-            data=rotation(data)
-            for i in data[cnt]: 
-                print(i,end='')    
-            print('',end=' ')
-        print()
-        data=rotation(data)
-        cnt+=1
+for t in range(1, int(input())):
+    n = int(input())
+    print(f'#{t}')
+    arr = [] # 배열넣기
+    for i in range(n):
+        arr.append(list(input().split()))
+        
+    answer=[[] for _ in range(n)] 
+    for _ in range(3):
+        arr=rotation()
+        for i in range(n):
+            answer[i].append(''.join(arr[i]))
 
-
+    for i in range(n):
+        print(*answer[i])
